2a3ce48a073d877aaa12ed5420a3ef0fec30270b
tejasmehta1 <tejasmehtag@gmail.com>
2020-06-28 16:01:16 -0500

    fixed animation with header

---
+++ src/components/work.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import ProfilePic from "../images/profilePic.jpg"
+import PropTypes from "prop-types"
+import Layout from "./layout"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+const Work = () => {
+
+  return(
+    <React.Fragment>
+      <div className={}></div>
+    </React.Fragment>
+  )
+}
+// Work.propTypes = {
+//   show: PropTypes.string.isRequired,
+// }
+export default Work
