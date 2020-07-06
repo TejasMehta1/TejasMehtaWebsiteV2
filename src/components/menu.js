d04bff4918dca390d9aa66bd5d9a18a17092a7bc
tejasmehta1 <tejasmehtag@gmail.com>
2020-07-06 12:46:26 -0500

    Added Projects to mobile nav

--- src/components/menu.js
+++ src/components/menu.js
@@ -20,10 +20,10 @@ const Menu = ({ open }) => {
       {/*</a>*/}
       <AnchorLink offset="250" id="aboutNavLink" className="ripple" href="#profilePic">About </AnchorLink>
       <AnchorLink offset="150" id="workNavLink" className="ripple" href="#workExperience">Work </AnchorLink>
-      <AnchorLink className="ripple" href="#hippl">Projects</AnchorLink>
+      <AnchorLink offset="150" id="projectsNavLink" className="ripple" href="#projects">Projects</AnchorLink>
       <AnchorLink offset='150' id="skillsNavLink" className="ripple"  href="#skillsLocation">Skills</AnchorLink>
       <AnchorLink offset='150' id="resumeNavLink" className="ripple" href="#resumeTitle">Resume</AnchorLink>
-      <AnchorLink offset='120' id="contactNavLink" className="ripple" href="#ContactUs">Contact</AnchorLink>
+      <AnchorLink offset='200' id="contactNavLink" className="ripple" href="#ContactUs">Contact</AnchorLink>
 
     </StyledMenu>
   )
