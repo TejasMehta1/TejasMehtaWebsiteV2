77c6a690379fa371e354df2979ebeb2fe742affc
tejasmehta1 <tejasmehtag@gmail.com>
2020-10-24 18:21:14 -0500

    Added playstore.js

---
+++ src/pages/playstore.js
@@ -0,0 +1,23 @@
+import React from "react"
+import SEO from "../components/seo"
+import {navigate} from 'gatsby';
+import "../components/tejas.css"
+class PlayStore extends React.Component {
+
+  componentDidMount() {
+    window.location = 'https://play.google.com/store/apps/developer?id=TejasMehta';
+    // navigate('../TejasMehtaResume.pdf');
+  }
+
+  render(){
+    return (
+      // <CADLayout>
+      <React.Fragment>
+        <SEO title="Play Store Apps" />
+      </React.Fragment>
+    )
+
+  }
+
+}
+export default PlayStore
